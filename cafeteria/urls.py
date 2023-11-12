4a4a3caf5173152ce3373710fe7d26da2ea429bc
Anderson Gonçalves <andersontno@gmail.com>
2023-11-12 11:28:59 -0300

    Adicionando nome para as urls e aplicando na chamada dos botões

--- cafeteria/urls.py
+++ cafeteria/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from cafeteria.views import *
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem),
-    path('imagem.html', imagem)
+    path('', index, name = "index"),
+    path('imagem/', imagem, name = "imagem"),
 ]
