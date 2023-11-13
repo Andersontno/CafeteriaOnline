80e196103a1c3a09b0e7e4e2454db064224bed2c
Anderson Gonçalves <andersontno@gmail.com>
2023-11-13 20:39:18 -0300

    Adicionando categorias ao produto

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from cafeteria.models import Produto
 
 class ListandoProdutos(admin.ModelAdmin):
-    list_display = ("id", "denominacao", "legenda")
+    list_display = ("id", "denominacao", "categoria", "legenda")
     list_display_links = ("id", "denominacao")
     search_fields = ("id", "denominacao")
 
--- cafeteria/models.py
+++ cafeteria/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Produto(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("EXPRESSO", "Café Expresso")
+        ,("CAPPUCCINO", "Cappuccino")
+        ,("ROBUSTA", "Café Robusta")
+        ,("EXTRAFORTE", "Café Extra Forte")
+    ]
     denominacao = models.CharField(max_length=100, null= False, blank=False)
     legenda = models.CharField(max_length=100, null= False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null=False, blank=False)
     imagem = models.CharField(max_length=100, null= False, blank=False)
 
