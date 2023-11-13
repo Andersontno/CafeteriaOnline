b5ad00665f94a97a8e3995e2ca2e6125df90246c
Anderson Gonçalves <andersontno@gmail.com>
2023-11-13 19:47:36 -0300

    Criação de estrutura de repetição no html e realizando as migrações

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'cafeteria/index.html')
+
+    dados = {
+        1:{"Nome": "Cafe Expresso",
+        "legenda": "Café Preto"},
+        2:{"Nome": "Cafezão",
+        "legenda": "Café Grande "},
+        3:{"Nome": "Cafezin",
+        "legenda": "Café Pequeno "},
+    }
+    
+    return render(request, 'cafeteria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'cafeteria/imagem.html')
--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cafeteria'
+    'cafeteria.apps.CafeteriaConfig'
 ]
 
 MIDDLEWARE = [
