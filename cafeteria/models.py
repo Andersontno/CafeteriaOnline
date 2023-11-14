9d09453222dc206dbeec22d0b358fe484b614bde
Anderson Gonçalves <andersontno@gmail.com>
2023-11-14 13:26:04 -0300

    Ajustes para pegar as imagens no html e salvar em um local diferente

--- cafeteria/models.py
+++ cafeteria/models.py
@@ -15,9 +15,9 @@ class Produto(models.Model):
     legenda = models.CharField(max_length=100, null= False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null=False, blank=False)
-    imagem = models.ImageField(upload_to="imagens/%Y/%m/%d/", blank=True)
+    imagem = models.ImageField(upload_to="imagens/produto/", blank=True)
     publicada = models.BooleanField(default=False)
     data_criacao = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f"Produto [denominacao={self.denominacao}]"
+        return self.denominacao
