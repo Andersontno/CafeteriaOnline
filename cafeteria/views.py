053ed6f20524bdaaa19b85a56ee34cdd6fc419fd
Anderson Gonçalves <andersontno@gmail.com>
2023-11-14 17:56:53 -0300

    Bloqueando acesso caso não esteja logado

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from cafeteria.models import Produto
 # Create your views here.
 
 def index(request): 
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")
+        return redirect('login')
+    
     produto = Produto.objects.order_by("-data_criacao").filter(publicada=True)
     return render(request, 'cafeteria/index.html', {"cards": produto})
 
@@ -11,6 +16,10 @@ def imagem(request, imagem_id):
     return render(request, 'cafeteria/imagem.html', {"produto": produto})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")        
+        return redirect('login')
+
     produto = Produto.objects.order_by("-data_criacao").filter(publicada=True)
 
     if "buscar" in request.GET:
